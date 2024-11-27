f9300466b4e0e6fbe209e641cabc33216b186e97
unknown <petra.j-son@outlook.com>
2024-11-27 21:10:02 +0100

    got info into local storage,looking like shit and do not know what to do att all

--- exercise-js-todo-list-main/index.js
+++ exercise-js-todo-list-main/index.js
@@ -1,16 +1,64 @@
 
 const input = document.querySelector(".input")
 const itemList = document.querySelector(".list")
-const formSubmit =document.querySelector("form")
+const formSubmit = document.querySelector("form")
+const box = document.querySelector(".toDo")
+
+//localStorage.clear();
 
 //Use input to add to the to do list
 
 formSubmit.addEventListener("submit", (event)=>{
         event.preventDefault();
+
         const li = document.createElement("li");
         li.textContent = input.value;
+
+        /*const ok = document.createElement("div");
+        ok.innerText = "check_box_outline_blank";
+        ok.setAttribute("class", "material-symbols-outlined");*/
+
         itemList.appendChild(li);
+        /*li.appendChild(ok);*/
+
+        const listLi = itemList.children;
+        const list = [];
+
+        for(i=0; i<listLi.length; i++){
+            const listObject ={
+                ToDoNr:i +1,
+                text:listLi[i].innerText
+            }
+            list.push(listObject);
+        }
+
+        localStorage.setItem("ToDoo", JSON.stringify(list));
+    
         formSubmit.reset();
+        
 })
 
 //Mark a todo as completed
+
+const items = localStorage.getItem("ToDoo");
+console.log(items)
+console.log(JSON.parse(items));
+
+
+//style
+box.style.color="black";
+box.style.background="gray";
+box.style.display="flex";
+box.style.flexDirection = "column";
+box.style.justifyContent= "center";
+box.style.width= "300px";
+
+itemList.style.display = "flex";
+itemList.style.flexDirection = "column";
+itemList.style.padding="20px"
+
+
+document.body.style.background= "darkgray";
+document.body.style.justifyContent = "center";
+
+
