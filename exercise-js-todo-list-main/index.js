e91ea6414fe139ceef60675f880df277b38ac76d
unknown <petra.j-son@outlook.com>
2024-12-01 08:13:36 +0100

    google icon 1 work nr not wayyyyyyyy not

--- exercise-js-todo-list-main/index.js
+++ exercise-js-todo-list-main/index.js
@@ -1,22 +1,18 @@
 const input = document.querySelector(".input");
-const inputValue = document.querySelector("#input-value");
 const ulList = document.querySelector(".ulList");
 const form = document.querySelector(".form");
 const box = document.querySelector(".container");
 
 const toDoList = []
 
-//const li = document.createElement("li");
-//ulList.appendChild(li);
+const ToDoListObject ={
+    ToDo: toDoList,
+    Done: [],
+}
 
-//const ok = `<span class="material-symbols-outlined">check_box_outline_blank</span>`;
-//ulList.insertAdjacentHTML("beforeend", ok);
 
 
-/*const ok = document.createElement("div");
-ok.innerText = "check_box_outline_blank";
-ok.setAttribute("class", "material-symbols-outlined");
-li.appendChild(ok);*/
+// add new todos to list and a checkbox
 
 input.addEventListener ("keypress",function(event){
     
@@ -29,13 +25,53 @@ input.addEventListener ("keypress",function(event){
       li.appendChild(ok);
       
       const text = document.createTextNode(input.value);
-      li.appendChild(text);
-      ulList.appendChild(li);
 
-      toDoList.push(li);
+      li.appendChild(text);
+      ulList.appendChild(li)
+ 
+      toDoList.push(li.lastChild.data);
+      
+      localStorage.setItem("ToDo", JSON.stringify(ToDoListObject));
       input.value=""
     }
   })
 
+// add a class to the checkbox
+  
+
+
+
+// click to done on things you complited
+
+  ulList.addEventListener("click", (event)=>{
     
-console.log(toDoList)
+    const okChecked = document.createElement("div");
+    okChecked.innerText = "check_box";
+    okChecked.setAttribute("class", "material-symbols-outlined");
+
+    const siblings = ulList.firstChild
+    const child = siblings.firstChild
+    console.log(siblings)
+    console.log(child)
+
+    if(siblings){
+        console.log("hello")
+        child.parentNode.replaceChild(okChecked, child);
+    }
+  })
+    
+// submit
+
+form.addEventListener("submit", (event) => {
+  
+});
+
+//access the local storage
+
+let storedSettings = JSON.parse(localStorage.getItem("ToDoListObject"));
+console.log(storedSettings)
+   
+//console.log(JSON.parse(ToDoListObject));
+
+
+//localStorage.clear();
